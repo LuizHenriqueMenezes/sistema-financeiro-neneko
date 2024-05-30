c4ea60086b15d30c44546f9c78efcf36bb2b69c5
Luiz H. Menezes <79934587+LuizHenriqueMenezes@users.noreply.github.com>
2024-05-29 21:36:36 -0300

    commit projeto

---
+++ neneko/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <title>Bem-vindo!!</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="icon" href="img/gatinho.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+<div class="barra">
+        <h1>Neneko <img src="img/gatinho.png" alt="logo"></h1>
+        <nav>
+            <ul>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="content">
+        <div class="corpo-index">
+            <h1 class="h1-index">Imagina poder gerenciar toda sua</h1>
+            <h1 class="h1-index">vida financeira em um só lugar?</h1>
+            <a href="login.php"><button type="button" class="btn btn-light">Organize suas economias com a gente!</button></a>
+        </div>
+        <div class="img-index">
+            <img src="img/notinha.png" alt="notinha-bea">
+        </div>
+    </div>
+</body>
+</html>
